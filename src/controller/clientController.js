1c4493955a3748e06f2e3e68bc9685e8d81cb2b8
eds9nsilva <antonioedson99@gmail.com>
2020-06-12 15:31:14 -0300

    complet

--- src/controller/clientController.js
+++ src/controller/clientController.js
@@ -1,20 +1,49 @@
-const connection = require('../models/connection')
+const connection = require('../models/connection');
+const { request } = require('express');
+const { destroy } = require('../models/connection');
+
 module.exports = {
     store(request, response) {
-      
-        const {name} = request.body;
 
-        connection.query(`INSERT INTO client SET ?`, {name}, function (err, result) {
+        const { name } = request.body;
+
+        connection.query(`INSERT INTO client SET ?`, { name }, function (err, result) {
             if (err) throw err;
             console.log("client successfully registered");
         });
         return response.json("client successfully registered");
     },
 
-    index(resquest, response){
+    index(resquest, response) {
         connection.query("SELECT * FROM client", function (err, result) {
             if (err) throw err;
             return response.json(result);
-          });
+        });
+    },
+
+    show(resquest, response) {
+        const Id = resquest.headers.id;
+        connection.query(`SELECT * FROM client WHERE id = ${Id}`, function (err, result) {
+            if (err) throw err;
+            return response.json(result);
+        })
+    },
+    update(resquest, response) {
+        const { name } = resquest.body;
+        const Id = resquest.headers.id;
+
+        connection.query(`UPDATE client SET name = ? WHERE id = ?`, [name, Id], function (err, result) {
+            if (err) throw err;
+            return response.json("Sucesso");
+        })
+
+    },
+
+    destroy(request, response){
+        const Id = request.headers.id;
+        connection.query("DELETE  FROM client WHERE Id = ?", Id, function(err,result){
+            if(err) throw err;
+            return response.json("Delete sucesso");
+        })
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 const {Router} = require('express');
-const clientController = require('./controller/clientController')
+const clientController = require('./controller/clientController');
 const routes = Router();
 
 routes.post('/client', clientController.store);
 routes.get('/listClient', clientController.index);
+routes.get('/searchClient', clientController.show);
+routes.put('/update', clientController.update);
+routes.delete('/delete', clientController.destroy);
+
+
 module.exports = routes;
